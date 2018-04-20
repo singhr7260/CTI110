1a973201892943cb1d1487b91c14edb7f0b32153
singhr7260 <38478494+singhr7260@users.noreply.github.com>
2018-04-20 12:04:16 -0400

    Add files via upload

---
+++ P2HW1_DistanceTraveled_singhritesh.py
@@ -0,0 +1,30 @@
+
+# CTI-110 
+# P2HW1 - Distance Traveled  
+# Your Name - Ritesh Singh
+# Date - 04/20/2018
+# Get the  distance
+
+
+# calculate the distance as car speed  is 70 mile per hours
+
+time6= 6
+time10= 10
+time15= 15
+speed = 70
+
+distance6= time6 * speed
+
+distance10= time10 * speed
+
+distance15= time15 * speed
+
+
+# display the distance After these time
+
+print("distance after ",time6,"is",distance6)
+
+print("distance after ",time10,"is",distance10)
+
+print("distance after ",time15,"is", distance15)
+      
